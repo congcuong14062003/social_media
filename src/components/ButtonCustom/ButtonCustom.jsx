f20d504ef6e68e45dddff2674acfbc70a32f1db1
ADMIN\Admin <cuongkoi1406@gmail.com>
2025-04-03 10:40:41 +0700

    test git

--- src/components/ButtonCustom/ButtonCustom.jsx
+++ src/components/ButtonCustom/ButtonCustom.jsx
@@ -1,7 +1,10 @@
 import { Button } from '@mui/material';
 import './ButtonCustom.scss';
+import { ClassNames } from '@emotion/react';
 function ButtonCustom({ title, onClick, startIcon, className, type, disabled, name }) {
     const classes = `btn_action_custom ${className}`;
+    console.log(className);
+    
     return (
         <Button name={name} disabled={disabled} type={type} onClick={onClick} className={classes} variant="contained" startIcon={startIcon}>
             {title}
